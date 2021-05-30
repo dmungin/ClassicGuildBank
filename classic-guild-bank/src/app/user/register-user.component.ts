2303a63b1b7989e48ca4fb8717346555e9aaafd8
David Mungin <david.mungin@gmail.com>
2021-05-30 12:45:53 -0400

    Updated registration message since send grid is not implemented for now

--- classic-guild-bank/src/app/user/register-user.component.ts
+++ classic-guild-bank/src/app/user/register-user.component.ts
@@ -71,7 +71,7 @@ export class RegisterUserComponent implements OnInit {
 
     this.userStore.registerUser(this.registerForm.value).subscribe({
       next: () =>{
-        this.successText = "An email has been sent to the address provided, you must confirm your account before logging in."
+        this.successText = "Please wait for the administrator to confirm your account before logging in."
         this.formSubmitted = true;
       }, 
       error: (errorResponse) => {
