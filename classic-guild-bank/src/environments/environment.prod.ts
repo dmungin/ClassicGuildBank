964c968a3d430a16d04bc2ce666a7e816ac9a023
David Mungin <david.mungin@gmail.com>
2021-05-30 12:17:30 -0400

    Upadated more api url references for azure

--- classic-guild-bank/src/environments/environment.prod.ts
+++ classic-guild-bank/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: "https://classicguildbankapi.azurewebsites.net/api",
-  client: "https://classicguildbank.com",
+  client: "https://classicguildbank.azurewebsites.net",
   patreon: {
     oauth_redirect: 'https://www.classicguildbank.com/patreon/callback',
     client_id: 'hmjgYD08W4nvMauZip0okmlof5C1bQWr1CfMCJ_pnccqtZobwBbDme4jSvyekSRh'
